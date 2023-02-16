e5b34bb80ffd5543fe1a4d41e0a6cf85519469b1
omokereoghenekaro <omokereoghenekaro@gmail.com>
2023-02-16 03:18:57 -0800

    update

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*
-*Return: always 0 (Success)
-*/
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	printf("this is a string")
-	return (0);
+    return (0);
 }
-
