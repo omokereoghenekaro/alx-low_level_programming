96525275e37a8644efa812b0c070e58722f5fdc6
omokereoghenekaro <omokereoghenekaro@gmail.com>
2023-02-17 01:31:00 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+/**
+ *this is a sample code style
+ */
 int main(void)
 {
 	char str_f[] = "\"Programming is like building a multilingual puzzle"; 
 	puts(str_f);
 	return 0;
-
 }
