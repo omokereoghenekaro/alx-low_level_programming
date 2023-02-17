8ff42cbbf7d2f98e3ac9aabe88c1ec4d95534ab1
omokereoghenekaro <omokereoghenekaro@gmail.com>
2023-02-17 14:28:36 -0800

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,16 +17,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_dig = n % 10;
-	
-	if (last_dig > 5) {
+	if (last_dig > 5)
 		strcpy(str, "and is greater than 5");
-	}
-	if (last_dig == 0) {
+	if (last_dig == 0)
 		strcpy(str, "and is 0");
-	}
-	if (last_dig <6 && last_dig != 0) {
+	if (last_dig <6 && last_dig != 0)
 		strcpy(str, "and is less than 6 and not 0");
-	}
-	printf("Last digit of %d is %d %s \n", n, last_dig, str);	
+	printf("Last digit of %d is %d %s", n, last_dig, str);	
 	return (0);
 }
