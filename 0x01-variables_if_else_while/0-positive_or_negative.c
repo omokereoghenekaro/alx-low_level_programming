0f7a96b595515da909e2433006399591ecc06e6b
omokereoghenekaro <omokereoghenekaro@gmail.com>
2023-02-17 12:48:20 -0800

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("is positive\n");
-	
 	if (n < 0)
 		printf("is negative\n");
-	
 	if (n == 0)
 		printf("is zero\n");
 	return (0);
