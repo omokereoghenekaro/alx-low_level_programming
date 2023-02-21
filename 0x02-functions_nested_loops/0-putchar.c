237926a510dd7834722bd335bfa0a5e578544215
omokereoghenekaro <omokereoghenekaro@gmail.com>
2023-02-21 14:06:28 -0800

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,6 +11,13 @@
  * */
 int main(void)
 {
-	_putchar("_putchar\n");
-	return (0);	
+	int i = 0;
+	char c[] = "_putchar";
+
+	for (int i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	putchar("\n");
+	return (0);
 }
