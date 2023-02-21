0fe52b68f5ec1499a53032f623458c4c84282be8
omokereoghenekaro <omokereoghenekaro@gmail.com>
2023-02-21 15:11:53 -0800

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * main- bla bla bka
+ * return - always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
