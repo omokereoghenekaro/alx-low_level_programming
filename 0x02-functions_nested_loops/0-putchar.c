b7d66ee0f9b8e8e5f21685e94ca5a2127cea5b66
omokereoghenekaro <omokereoghenekaro@gmail.com>
2023-02-21 14:25:24 -0800

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
 /**
  ** _putchar - writes the character c to stdout
  ** @c: The character to print
- **
+ ** main - the main block
  ** Return: On success 1.
  ** On error, -1 is returned, and errno is set appropriately.
  **/
