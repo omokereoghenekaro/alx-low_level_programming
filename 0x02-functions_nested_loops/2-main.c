dea04b1fe963578fe7de5e385997b91eb0e1aee9
omokereoghenekaro <omokereoghenekaro@gmail.com>
2023-02-21 16:05:44 -0800

    update

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ ** print_alphabet_x10 - writes the character c to stdout
+ **
+ ** Return: On success 1.
+ ** On error, -1 is returned, and errno is set appropriately.
+ **/
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ ** print_alphabet_x10 - writes the character c to stdout
+ **
+ ** Return: On success 1.
+ ** On error, -1 is returned, and errno is set appropriately.
+ **/
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+	while (i <= 9)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
